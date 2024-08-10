d2f360548744fa785eb9810b8f04f4cc573d9f5f
김동균 <wooheuy5752@nate.com>
2024-08-10 17:21:53 +0900

    chore : 화면 크기 설정
    모바일 기준 최소 최대 너비 설정
    웹 기준 가운데 정렬
    
    #13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,12 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head />
-      <body>
-        <QueryProvider>{children}</QueryProvider>
+      <body className="flex-center">
+        <QueryProvider>
+          <div className="relative h-svh w-full min-w-[320px] max-w-[450px] overflow-hidden border-x">
+            {children}
+          </div>
+        </QueryProvider>
       </body>
     </html>
   )
