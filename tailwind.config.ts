c6c92a07e6656450cf1d7273b3cde80a01b4887f
김동균 <wooheuy5752@nate.com>
2024-08-10 17:06:16 +0900

    chore : 그림자 속성 추가 #13

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,11 @@ const config: Config = {
         body: '24px',
         caption: '18px',
       },
+      boxShadow: {
+        lightShadow: '0px 0px 12px 0px rgba(2, 41, 34, 0.10)',
+        topShadow: '0px -4px 12px 0px rgba(0, 0, 0, 0.10))',
+        underShadow: '0px 2px 4px 0px rgba(2, 41, 34, 0.10)',
+      },
     },
   },
   plugins: [],
