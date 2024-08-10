38b8e42053417125a64a50af60f69f10eb760e40
김동균 <wooheuy5752@nate.com>
2024-08-10 16:51:05 +0900

    chore : 컬러 팔레트 설정
    컬러 팔레트 설정
    globals.css 내부 tailwind 속성 분리
    #13

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,37 @@
 import type { Config } from 'tailwindcss'
 
+const COLORS = {
+  mint: {
+    1: '#C5FDEC',
+    2: '#98F9DC',
+    3: '#50E9BB',
+    4: '#27CD9B',
+    5: '#1BB889',
+    6: '#13A076',
+    7: '#0E8763',
+    8: '#035344',
+    9: '#022922',
+  },
+  green: {
+    1: '#A9FEC5',
+    2: '#61F191',
+    3: '#40D270',
+    4: '#35BD62',
+    5: '#2CA655',
+  },
+  gray: {
+    1: '#F5F6F8',
+    2: '#EAECEE',
+    3: '#DCDFE0',
+    4: '#CBCCCE',
+    5: '#B4B5B8',
+    6: '#949698',
+    7: '#676A6B',
+    8: '#3B3C3D',
+  },
+  red: '#DA242F',
+}
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,9 +42,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-mint': 'linear-gradient(180deg, #50E9BB 0%, #40D270 100%)',
       },
+      backgroundColor: { ...COLORS },
+      colors: { ...COLORS },
     },
   },
   plugins: [],
