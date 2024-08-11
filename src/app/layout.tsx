1eb6270b2c3efc6507f0ae1831d968466070a6b3
김동균 <wooheuy5752@nate.com>
2024-08-11 17:58:47 +0900

    chore : 폰트 설정 변경
    폰트를 추가하는 기존의 방식에서 구글 폰트를 자동으로 가져오는 방식으로 변경
    
    #13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import type { PropsWithChildren } from 'react'
+import { Noto_Sans_KR } from 'next/font/google'
 
 import QueryProvider from '@/hooks/QueryProvider'
 
 import './globals.css'
 
+const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['400', '500', '700'] })
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
+    <html lang="en" className={notoSans.className}>
       <head />
-      <body className="flex-center">
+      <body className="flex-center font-medium">
         <QueryProvider>
           <div className="relative h-svh w-full min-w-[320px] max-w-[450px] overflow-hidden border-x">
             {children}
