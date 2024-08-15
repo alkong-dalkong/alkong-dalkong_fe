b72345b9952939f49f832443841326c0d73acc8e
g0mteng <gomteng03@naver.com>
2024-08-15 20:27:02 +0900

    feat: Tag Component (#26)

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,3 +1,19 @@
-export default function Tag() {
-  return <div>Tag</div>
+interface ITagProps {
+  children?: string
+  color?: 'primary' | 'add' | 'delete'
+  onClick?: () => void
+}
+
+export default function Tag({ children = '', color = 'primary', onClick }: ITagProps) {
+  const colors = {
+    primary: 'bg-mint-3 text-black',
+    add: 'bg-gray-2 text-gray-7',
+    delete: 'bg-mint-6 text-white',
+  }
+
+  return (
+    <button onClick={onClick} className={`${colors[color]} rounded-[99px] px-[11px] py-[5px]`}>
+      {children}
+    </button>
+  )
 }
