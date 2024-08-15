e66514653c02292f64ef42bfa5b502bc50fd3c0f
g0mteng <gomteng03@naver.com>
2024-08-15 20:27:34 +0900

    test: Storybook Tag Component (#26)

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryFn } from '@storybook/react'
+
+import Tag from './Tag'
+
+export default {
+  title: 'Components/Tag',
+  component: Tag,
+  argTypes: {
+    color: {
+      control: 'select',
+      options: ['primary', 'add', 'delete'],
+    },
+    children: { control: 'text' },
+    onClick: { action: 'clicked' },
+  },
+} as Meta
+
+const Template: StoryFn = (args) => <Tag {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  children: '건강검진',
+  color: 'primary',
+}
+
+export const Add = Template.bind({})
+Add.args = {
+  children: '+ 속쓰림',
+  color: 'add',
+}
+
+export const Delete = Template.bind({})
+Delete.args = {
+  children: '- 건강검진',
+  color: 'delete',
+}
