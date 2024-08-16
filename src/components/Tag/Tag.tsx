44769be5514aaab4b1874ad985a981512768d7b7
g0mteng <gomteng03@naver.com>
2024-08-16 10:25:05 +0900

    style: move colors outside of component (#26)

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -4,15 +4,15 @@ interface ITagProps {
   onClick?: () => void
 }
 
-export default function Tag({ children = '', color = 'primary', onClick }: ITagProps) {
-  const colors = {
-    primary: 'bg-mint-3 text-black',
-    add: 'bg-gray-2 text-gray-7',
-    delete: 'bg-mint-6 text-white',
-  }
+const TagColors = {
+  primary: 'bg-mint-3 text-black',
+  add: 'bg-gray-2 text-gray-7',
+  delete: 'bg-mint-6 text-white',
+}
 
+export default function Tag({ children = '', color = 'primary', onClick }: ITagProps) {
   return (
-    <button onClick={onClick} className={`${colors[color]} rounded-[99px] px-[11px] py-[5px]`}>
+    <button onClick={onClick} className={`${TagColors[color]} rounded-[99px] px-[11px] py-[5px]`}>
       {children}
     </button>
   )
