5d95d0f5ac9a41c056f7c23505c2755a02e4575d
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-21 02:39:06 +0900

    feat: Icon 컴포넌트 추가 (#33)

---
+++ src/components/icons/Check.tsx
@@ -0,0 +1,40 @@
+import type { IconProps } from '.'
+
+export const CheckNoIcon = (props: IconProps) => {
+  const { color = '#B4B5B8', size = 36 } = props
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 36 36"
+      fill="none"
+    >
+      <circle cx="18" cy="18" r="13.5" stroke={color} stroke-dasharray="2 2" />
+    </svg>
+  )
+}
+
+export const CheckYesIcon = (props: IconProps) => {
+  const { color = '#13A076', size = 36 } = props
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 36 36"
+      fill="none"
+    >
+      <circle cx="18" cy="18" r="14" fill={color} />
+      <path
+        d="M26 14L16.9565 25L10 18.4"
+        stroke="white"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,53 @@
+import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, ArrowUpIcon } from './Arrow'
+import { CheckNoIcon, CheckYesIcon } from './Check'
+import { CloseIcon } from './Close'
+import {
+  CalendarLabelIcon,
+  CheckLabelIcon,
+  ClinicLabelIcon,
+  EmergencyLabelIcon,
+  HealthLabelIcon,
+  MedicineLabelIcon,
+  TimeLabelIcon,
+} from './Label'
+import { MinusIcon } from './Minus'
+import { ClinicIcon, HealthIcon, HomeIcon, MedicineIcon } from './Navi'
+import { PlusIcon } from './Plus'
+
+export type IconProps = {
+  color?: string
+  size?: number
+}
+
+export const iconMap = {
+  'arrow-down': ArrowDownIcon,
+  'arrow-up': ArrowUpIcon,
+  'arrow-left': ArrowLeftIcon,
+  'arrow-right': ArrowRightIcon,
+  'check-no': CheckNoIcon,
+  'check-yes': CheckYesIcon,
+  close: CloseIcon,
+  plus: PlusIcon,
+  minus: MinusIcon,
+  'health-label': HealthLabelIcon,
+  'medicine-label': MedicineLabelIcon,
+  'clinic-label': ClinicLabelIcon,
+  'check-label': CheckLabelIcon,
+  'time-label': TimeLabelIcon,
+  'emergency-label': EmergencyLabelIcon,
+  'calendar-label': CalendarLabelIcon,
+  home: HomeIcon,
+  medicine: MedicineIcon,
+  clinic: ClinicIcon,
+  health: HealthIcon,
+}
+
+export type IconComponentProps = IconProps & {
+  name: keyof typeof iconMap
+}
+
+export const Icon = ({ name, ...props }: IconComponentProps) => {
+  const IconComponent = iconMap[name]
+
+  return <IconComponent {...props} />
+}
