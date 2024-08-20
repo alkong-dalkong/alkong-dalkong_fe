0f95d498a1b25b62e024de557ba629cf86325abb
김동균 <wooheuy5752@nate.com>
2024-08-20 20:05:28 +0900

    chore : Label 타입명 및 속성 변경
    Props에서 LabelProps로 이름 변경
    children을 ReactNode로 받는 것이 아닌 PropsWithChildren 사용
    
    #36

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,11 +1,11 @@
+import type { PropsWithChildren } from 'react'
 import Image from 'next/image'
 
-type Props = {
-  children: React.ReactNode
+type LabelProps = {
   src?: string
 }
 
-const Label = ({ children, src }: Props) => {
+const Label = ({ children, src }: PropsWithChildren<LabelProps>) => {
   return (
     <div className="flex-align gap-2">
       {src && <Image src={src} width={28} height={28} alt="label-icon" />}
