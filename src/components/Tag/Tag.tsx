e9aaec6988afd3fb2095dea0331c274825042665
g0mteng <gomteng03@naver.com>
2024-08-20 22:31:57 +0900

    feat: Tag 컴포넌트 return 타입 지정 (#26)

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -2,9 +2,9 @@ type Props = {
   label: string
 }
 
-const Tag = ({ label: title }: Props) => {
+const Tag: React.FC<Props> = ({ label }) => {
   return (
-    <span className="body-M rounded-[99px] bg-mint-3 px-[11px] py-[5px] text-black">{title}</span>
+    <span className="body-M rounded-[99px] bg-mint-3 px-[11px] py-[5px] text-black">{label}</span>
   )
 }
 
