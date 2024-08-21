4b98ca1e928786115601588a6ff173d701c74aa5
g0mteng <gomteng03@naver.com>
2024-08-21 11:43:11 +0900

    test: Modal 컴포넌트 스토리북 추가 (#44)

---
+++ src/components/modal/Modal.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryFn } from '@storybook/react'
+
+import { useModal } from '@/hooks/useModal'
+
+import Modal from './Modal'
+
+type Props = {
+  isOpen: boolean
+  onClose: () => void
+  size: 'md' | 'lg'
+}
+
+export default {
+  title: 'components/modal/Modal',
+  component: Modal,
+} as Meta
+
+const MediumTemplate: StoryFn<Props> = () => {
+  const [modal, handleOpenModal, handleCloseModal] = useModal(false)
+
+  return (
+    <>
+      <button onClick={handleOpenModal}>모달</button>
+      <Modal isOpen={modal} onClose={handleCloseModal} size="md">
+        <Modal.Title title="삭제하시겠습니까?" />
+        <Modal.SubTitle title="삭제하실 경우 복원이 불가능합니다." />
+        <div className="mt-[24px] flex gap-[15px]">
+          <div className="flex-center h-[52px] w-[140px] rounded-xl  bg-gray-3 text-gray-7 active:scale-95 active:bg-gray-4">
+            취소
+          </div>
+          <div className="flex-center h-[52px] w-[140px] rounded-xl bg-mint-6 text-white active:scale-95 active:bg-mint-7">
+            삭제
+          </div>
+        </div>
+      </Modal>
+    </>
+  )
+}
+
+export const Medium = MediumTemplate.bind({})
