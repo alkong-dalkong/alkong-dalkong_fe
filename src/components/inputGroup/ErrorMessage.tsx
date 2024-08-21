9c66aabc764050d7ea0cb302e9998fecb57dfbdb
김동균 <wooheuy5752@nate.com>
2024-08-21 23:23:07 +0900

    feat : 에러메세지 구현
    #50

---
+++ src/components/inputGroup/ErrorMessage.tsx
@@ -0,0 +1,19 @@
+import { useFormContext } from 'react-hook-form'
+
+type ErrorMessageType = {
+  section: string
+}
+
+export const ErrorMessage = ({ section }: ErrorMessageType) => {
+  const {
+    formState: { errors },
+  } = useFormContext()
+
+  return (
+    <>
+      {errors && errors[section] && errors[section].message && (
+        <p className="caption-M h-[18px] text-red">* {errors[section].message.toString()}</p>
+      )}
+    </>
+  )
+}
