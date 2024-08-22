1684394d99c47c56c921502860923225c1955e1f
김동균 <wooheuy5752@nate.com>
2024-08-22 14:32:46 +0900

    feat : InputWithoutRegister 구현
    #50

--- src/components/inputGroup/Input.tsx
+++ src/components/inputGroup/Input.tsx
@@ -8,6 +8,7 @@ type InputProps = {
   placeholder: string
 }
 
+type InputWithoutRegisterProps = Pick<InputProps, 'type' | 'placeholder'>
 type TextAreaProps = Omit<InputProps, 'type'>
 
 export const Input = ({ type = 'text', section, readOnly = false, placeholder }: InputProps) => {
@@ -18,6 +19,16 @@ export const Input = ({ type = 'text', section, readOnly = false, placeholder }:
       type={type}
       {...register(section)}
       readOnly={readOnly}
+      className="subtitle-M placeholder:subtitle-R w-full rounded-xl border border-mint-3 px-6 py-4 placeholder:text-gray-7 focus:outline-none"
+      placeholder={placeholder}
+    />
+  )
+}
+
+export const InputWithoutRegister = ({ type = 'text', placeholder }: InputWithoutRegisterProps) => {
+  return (
+    <input
+      type={type}
       className="subtitle-M placeholder:subtitle-R w-full rounded-xl border border-mint-3 px-6 py-4 placeholder:text-gray-7 focus:outline-none"
       placeholder={placeholder}
     />
--- src/components/inputGroup/index.ts
+++ src/components/inputGroup/index.ts
@@ -2,12 +2,13 @@ import { CheckBox, CheckBoxAll } from './CheckBox'
 import { Container } from './Container'
 import { ErrorMessage } from './ErrorMessage'
 import { Gender } from './Gender'
-import { Input, TextArea } from './Input'
+import { Input, InputWithoutRegister, TextArea } from './Input'
 import Stepper from './Stepper'
 import { TextWithArrow } from './TextWithArrow'
 
 export const InputGroup = Object.assign(Container, {
   Input,
+  InputWithoutRegister,
   TextArea,
   Gender,
   CheckBox,
