dc3d3aceb09ed8eda0e4c043ff14600942c1332d
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-22 22:23:21 +0900

    feat: framer-motion 재설정 (#39)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { PropsWithChildren } from 'react'
 import { Noto_Sans_KR } from 'next/font/google'
-import { domMax, LazyMotion } from 'framer-motion'
 
-import QueryProvider from '@/hooks/QueryProvider'
+import { LazyMotionProvider, QueryProvider } from '@/hooks'
 
 import './globals.css'
 
@@ -18,11 +17,11 @@ export default function RootLayout({ children }: PropsWithChildren) {
       <head />
       <body className="flex-center font-medium">
         <QueryProvider>
-          <LazyMotion features={domMax}>
+          <LazyMotionProvider>
             <div className="relative h-svh w-full min-w-[320px] max-w-[450px] overflow-y-scroll border-x scrollbar-hide">
               {children}
             </div>
-          </LazyMotion>
+          </LazyMotionProvider>
         </QueryProvider>
       </body>
     </html>
