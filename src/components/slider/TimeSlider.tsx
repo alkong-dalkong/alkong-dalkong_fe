a639f754e7947ac9abf86207dd8cfe1f34e779e3
김동균 <wooheuy5752@nate.com>
2024-08-24 15:13:06 +0900

    feat : TimeSlider에 timeStore 적용
    #42

--- src/components/slider/TimeSlider.tsx
+++ src/components/slider/TimeSlider.tsx
@@ -1,16 +1,20 @@
+import { useSelectedTimeActions } from '@/store/timeStore'
+
 import { Slider } from './Slider'
 
 export const TimeSlider = () => {
   const HOURS = Array.from({ length: 24 }, (_, i) => String(i).padStart(2, '0'))
   const MINUTES = Array.from({ length: 60 }, (_, i) => String(i).padStart(2, '0'))
 
+  const { handleHourChange, handleMinuteChange } = useSelectedTimeActions()
+
   return (
     <div className="flex-center relative rounded-xl bg-mint-0 py-[17px]">
       <div className="absolute top-1/2 h-[30px] w-[192px] -translate-y-1/2 rounded-lg bg-mint-2" />
       <div className="flex gap-8">
-        <Slider list={HOURS} />
+        <Slider list={HOURS} onChange={handleHourChange} />
         <p className="h-[30px] translate-y-[65px]">:</p>
-        <Slider list={MINUTES} />
+        <Slider list={MINUTES} onChange={handleMinuteChange} />
       </div>
     </div>
   )
