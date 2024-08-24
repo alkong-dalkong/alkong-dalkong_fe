c0ec72e56e7c5bdf2e046704e7f0497619d7e945
김동균 <wooheuy5752@nate.com>
2024-08-24 16:16:16 +0900

    chore : 슬라이더 스타일 추가
    전체 너비 설정
    유저 텍스트 선택 방지
    
    #42

--- src/components/slider/TimeSlider.tsx
+++ src/components/slider/TimeSlider.tsx
@@ -10,7 +10,7 @@ export const TimeSlider = () => {
   const { handleHourChange, handleMinuteChange } = useSelectedTimeActions()
 
   return (
-    <div className="flex-center relative rounded-xl bg-mint-0 py-[17px]">
+    <div className="flex-center relative w-full select-none rounded-xl bg-mint-0 py-[17px]">
       <div className="absolute top-1/2 h-[30px] w-[192px] -translate-y-1/2 rounded-lg bg-mint-2" />
       <div className="flex gap-8">
         <Slider list={HOURS} onChange={handleHourChange} />
--- src/components/slider/WeightSlider.tsx
+++ src/components/slider/WeightSlider.tsx
@@ -9,7 +9,7 @@ export const WeightSlider = () => {
     useSelectedWeightActions()
 
   return (
-    <div className="flex-center relative rounded-xl bg-mint-0 py-[17px]">
+    <div className="flex-center relative w-full select-none rounded-xl bg-mint-0 py-[17px]">
       <div className="headline-M absolute top-1/2 h-[30px] w-[158px] -translate-x-6 -translate-y-1/2 rounded-lg bg-mint-2" />
       <div className="flex gap-[14px]">
         <Slider list={WEIGHT_NUMBERS} onChange={handleTensWeightChange} />
