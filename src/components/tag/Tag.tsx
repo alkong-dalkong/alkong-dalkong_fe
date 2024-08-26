0695c438c970f161ff21c64017ed63b9f154668e
g0mteng <gomteng03@naver.com>
2024-08-26 12:21:18 +0900

    style: 태그 컴포넌트 Props 타입 수정 (#26)

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -2,7 +2,7 @@ type Props = {
   label: string
 }
 
-const Tag: React.FC<Props> = ({ label }) => {
+const Tag = ({ label }: Props) => {
   return (
     <span className="body-M rounded-[99px] bg-mint-3 px-[11px] py-[5px] text-black">{label}</span>
   )
