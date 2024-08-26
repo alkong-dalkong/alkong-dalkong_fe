d3045731a4aeceaa799109a13996cd397280ba08
김동균 <wooheuy5752@nate.com>
2024-08-26 10:32:33 +0900

    chore : Container row 변수명 isRow으로 수정
    #50

--- src/components/inputGroup/Container.tsx
+++ src/components/inputGroup/Container.tsx
@@ -1,10 +1,10 @@
 import type { PropsWithChildren } from 'react'
 
 type ContainerProps = {
-  row?: boolean
+  isRow?: boolean
 }
 
-export const Container = ({ row = false, children }: PropsWithChildren<ContainerProps>) => {
-  const layoutStyle = row ? 'flex-between-align' : 'flex-column gap-2'
+export const Container = ({ isRow = false, children }: PropsWithChildren<ContainerProps>) => {
+  const layoutStyle = isRow ? 'flex-between-align' : 'flex-column gap-2'
   return <div className={layoutStyle}>{children}</div>
 }
