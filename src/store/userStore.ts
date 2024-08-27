40a2d80ba49e22ffc77a9d11bbe3679c0da5f2ba
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-27 20:19:06 +0900

    feat: useUserStore 추가 (#41)

---
+++ src/store/userStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand'
+
+import type { User } from '@/types'
+
+type UserState = {
+  user: User
+}
+
+type UserActions = {
+  setUser: (user: User) => void
+}
+
+// mockData. 로그인 개발 이후 수정 필요
+const defaultState = {
+  userId: 'test',
+  username: '가나다라',
+  family: [
+    { userId: '1', username: '가나다라' },
+    { userId: '2', username: '마바사' },
+    { userId: '3', username: '아자차카' },
+    { userId: '4', username: '타파하' },
+  ],
+}
+
+export const useUserStore = create<UserState & UserActions>((set) => ({
+  user: defaultState,
+  setUser: (user: User) => {
+    set({ user })
+  },
+}))
