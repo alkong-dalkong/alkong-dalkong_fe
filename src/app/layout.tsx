de6cdf7930f86347815bb86622bfea8e27597374
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-27 22:01:06 +0900

    feat: BottomNav 적용 (#41)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from 'react'
 
+import { BottomNav } from '@/components'
 import { LazyMotionProvider, QueryProvider } from '@/hooks'
 
 import { notoSansKR } from '../../public/app/font'
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
           <LazyMotionProvider>
             <div className="relative h-svh w-full min-w-[320px] max-w-[450px] overflow-y-scroll border-x scrollbar-hide">
               {children}
+              <BottomNav />
             </div>
           </LazyMotionProvider>
         </QueryProvider>
