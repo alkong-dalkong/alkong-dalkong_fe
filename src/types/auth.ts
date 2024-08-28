86a23882086d7c2846e9e0bc076d27904819fce8
김동균 <wooheuy5752@nate.com>
2024-08-28 12:56:18 +0900

    feat : 회원가입 폼 스키마 작성
    #71

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,3 +2,15 @@ export type LoginFormType = {
   id: string
   password: string
 }
+
+export type SignupFormType = {
+  name: string
+  id: string
+  password: string
+  birth: string
+  phoneNumber: string
+  gender: 'MAN' | 'WOMAN'
+  confirm: boolean
+  personal: boolean
+  notification: boolean
+}
