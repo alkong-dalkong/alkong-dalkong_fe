251cb78bcf91d69e21ddd355b62dbce29a42d93b
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-29 15:57:20 +0900

    feat: 프로필 모달 컴포넌트 변경 (#41)
    - fixed -> absolute
    - font 유틸리티 클래스 오타 수정
    - w-screen -> wfull
    - justify-center 설정

--- src/components/profile/ProfileModal.tsx
+++ src/components/profile/ProfileModal.tsx
@@ -26,7 +26,7 @@ export const ProfileModal = ({ onClickProfileModal }: ProfileModalProps) => {
       <button
         type="button"
         onClick={handleClickScrim}
-        className={`fixed inset-0 flex h-[calc(100vh-75px)] w-screen items-end overflow-hidden bg-[rgba(15,23,42,0.5)] px-[52px] pb-[62px]`}
+        className={`absolute inset-0 flex h-[calc(100vh-75px)] w-full items-end justify-center overflow-hidden bg-[rgba(15,23,42,0.5)] px-[52px] pb-[62px]`}
       >
         <button type="button" onClick={handleClickScrim} className="grid grid-cols-3 gap-[40px]">
           {user.family?.map(({ userId, username }) => {
@@ -36,7 +36,7 @@ export const ProfileModal = ({ onClickProfileModal }: ProfileModalProps) => {
             }
 
             return (
-              <div className="Headline-B flex-column-align gap-y-[6px] text-white" key={userId}>
+              <div className="headline-B flex-column-align gap-y-[6px] text-white" key={userId}>
                 {username}
                 <Link href={`/home/${userId}`}>
                   <Profile name={username} size="lg" onClickProfile={handleClickProfile} />
