2c55d3941d79b8a79ff3d00b4f7f912149b090b8
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-29 22:39:59 +0900

    refactor: MainHeader 컴포넌트 변경 (#51)
    - PR 리뷰 반영하여 3가지 버전으로 분리

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -5,55 +5,59 @@ import { Profile } from '../profile/Profile'
 
 import type { HeaderProps } from './SubHeader'
 
-export const MainHeader = ({
-  title,
-  canBack,
-  canClose,
-  onClose,
-  btnStyle = 'icon',
-}: HeaderProps) => {
-  const router = useRouter()
-  const handleBack = () => {
-    router.back()
-  }
+const username = '가나다라' // 추후 useUserStore 로 변경 필요
 
-  const username = '가나다라' // 추후 useUserStore 로 변경 필요
-
-  const isIcon = btnStyle === 'icon'
-  const isMixed = btnStyle === 'mixed'
-
+export const Setting = ({ title, onClose }: HeaderProps) => {
   return (
     <header className="flex-column-between h-[182px] bg-mint-3 px-[20px] pb-[24px] pt-[20px]">
-      <div className="flex-between-align w-full">
-        {
-          <button
-            type="button"
-            onClick={isIcon || isMixed ? handleBack : onClose?.[0]}
-            className="body-B text-mint-7"
-          >
-            {canBack ? <Icon name="arrow-left" /> : !isIcon && '취소'}
-          </button>
-        }
-        {canClose && !isMixed ? (
-          <button
-            type="button"
-            onClick={isIcon ? onClose?.[0] : onClose?.[1]}
-            className="body-B text-mint-9"
-          >
-            {isIcon ? <Profile name={username} size="sm" bgColor="#C5FDEC" /> : '완료'}
-          </button>
-        ) : (
-          <div className="flex-align gap-[16px]">
-            <button type="button" onClick={onClose?.[0]} className="body-B text-mint-7">
-              삭제
-            </button>
-            <button type="button" onClick={onClose?.[1]} className="body-B text-mint-9">
-              완료
-            </button>
-          </div>
-        )}
+      <div className="flex-align w-full justify-end">
+        <Profile name={username} size="sm" bgColor="#C5FDEC" onClickProfile={onClose?.[0]} />
       </div>
       <h1 className="title-B whitespace-pre text-black">{title}</h1>
     </header>
   )
 }
+
+export const Confirm = ({ title, onClose }: HeaderProps) => {
+  return (
+    <header className="flex-column-between h-[182px] bg-mint-3 px-[20px] pb-[24px] pt-[20px]">
+      <div className="flex-between-align w-full">
+        <button className="body-B text-mint-7" onClick={onClose?.[0]}>
+          취소
+        </button>
+        <button className="body-B text-mint-9" onClick={onClose?.[1]}>
+          완료
+        </button>
+      </div>
+      <h1 className="title-B whitespace-pre text-black">{title}</h1>
+    </header>
+  )
+}
+
+export const BackAll = ({ title, onClose }: HeaderProps) => {
+  const router = useRouter()
+  const handleBack = () => {
+    router.back()
+  }
+
+  return (
+    <header className="flex-column-between h-[182px] bg-mint-3 px-[20px] pb-[24px] pt-[20px]">
+      <div className="flex-between-align w-full">
+        <button className="body-B text-mint-7" onClick={handleBack}>
+          <Icon name="arrow-left" />
+        </button>
+        <div className="flex-align gap-[16px]">
+          <button type="button" onClick={onClose?.[0]} className="body-B text-mint-7">
+            삭제
+          </button>
+          <button type="button" onClick={onClose?.[1]} className="body-B text-mint-9">
+            완료
+          </button>
+        </div>
+      </div>
+      <h1 className="title-B whitespace-pre text-black">{title}</h1>
+    </header>
+  )
+}
+
+export const MainHeader = { Setting, Confirm, BackAll }
