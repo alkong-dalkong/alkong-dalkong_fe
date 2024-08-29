85c98f8d59e05b28ee8a9a2c72a2a8bf2953d26c
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-29 22:40:30 +0900

    refactor: Header 컴포넌트 스토리 변경 (#51)
    - PR 리뷰 반영해서 변경된 컴포넌트 스토리 추가

--- src/components/header/Header.stories.tsx
+++ src/components/header/Header.stories.tsx
@@ -3,47 +3,39 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { MainHeader } from './MainHeader'
 import { SubHeader } from './SubHeader'
 
-const meta: Meta<typeof SubHeader> = {
+const meta: Meta<typeof SubHeader.Back> = {
   title: 'Header',
-  component: SubHeader,
+  component: SubHeader.Confirm,
   tags: ['autodocs'],
 }
 
 export default meta
 
-type Story = StoryObj<typeof SubHeader>
+type Story = StoryObj<typeof SubHeader.Back>
 
 export const Default: Story = {
   args: {
     title: '헤더 텍스트',
-    canBack: true,
   },
 }
 
 export const All = () => {
   return (
     <div className="flex-column gap-[20px]">
-      <SubHeader title="뒤로가기 아이콘 헤더" canBack />
-      <SubHeader title="닫기 아이콘 헤더" canClose onClose={[() => alert('닫기')]} />
-      <SubHeader
-        title="취소 + 완료 헤더"
-        canClose
-        onClose={[() => alert('취소'), () => alert('완료')]}
-        btnStyle="text"
-      />
-      <MainHeader title={`메인 헤더\n프로필 아이콘`} canClose onClose={[() => alert('프로필')]} />
-      <MainHeader
+      <MainHeader.Setting title={`메인 헤더\n프로필 아이콘`} onClose={[() => alert('프로필')]} />
+      <MainHeader.Confirm
         title={`메인 헤더\n취소 + 완료`}
-        canClose
         onClose={[() => alert('취소'), () => alert('완료')]}
-        btnStyle="text"
       />
-      <MainHeader
+      <MainHeader.BackAll
         title={`메인 헤더\n뒤로가기 아이콘 + 수정 + 삭제`}
-        canBack
-        canClose
         onClose={[() => alert('삭제'), () => alert('완료')]}
-        btnStyle="mixed"
+      />
+      <SubHeader.Back title="뒤로가기 아이콘 헤더" />
+      <SubHeader.Close title="닫기 아이콘 헤더" onClose={[() => alert('닫기')]} />
+      <SubHeader.Confirm
+        title="취소 + 완료 헤더"
+        onClose={[() => alert('취소'), () => alert('완료')]}
       />
     </div>
   )
