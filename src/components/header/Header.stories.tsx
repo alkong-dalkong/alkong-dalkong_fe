46feab5c29505f3b967378dc3fd2e590fec254ca
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-30 00:33:53 +0900

    refactor: Header 컴포넌트 스토리 변경 (#51)
    - PR 리뷰 반영해서 변경된 컴포넌트 스토리 추가

--- src/components/header/Header.stories.tsx
+++ src/components/header/Header.stories.tsx
@@ -5,7 +5,7 @@ import { SubHeader } from './SubHeader'
 
 const meta: Meta<typeof SubHeader.Back> = {
   title: 'Header',
-  component: SubHeader.Confirm,
+  component: SubHeader.Back,
   tags: ['autodocs'],
 }
 
@@ -22,20 +22,23 @@ export const Default: Story = {
 export const All = () => {
   return (
     <div className="flex-column gap-[20px]">
-      <MainHeader.Setting title={`메인 헤더\n프로필 아이콘`} onClose={[() => alert('프로필')]} />
+      <MainHeader.Setting title={`메인 헤더\n프로필 아이콘`} onSet={() => alert('마이페이지')} />
       <MainHeader.Confirm
         title={`메인 헤더\n취소 + 완료`}
-        onClose={[() => alert('취소'), () => alert('완료')]}
+        onCancel={() => alert('취소')}
+        onConfirm={() => alert('완료')}
       />
-      <MainHeader.BackAll
+      <MainHeader.Modify
         title={`메인 헤더\n뒤로가기 아이콘 + 수정 + 삭제`}
-        onClose={[() => alert('삭제'), () => alert('완료')]}
+        onDelete={() => alert('삭제')}
+        onModify={() => alert('수정')}
       />
       <SubHeader.Back title="뒤로가기 아이콘 헤더" />
-      <SubHeader.Close title="닫기 아이콘 헤더" onClose={[() => alert('닫기')]} />
+      <SubHeader.Close title="닫기 아이콘 헤더" onClose={() => alert('닫기')} />
       <SubHeader.Confirm
         title="취소 + 완료 헤더"
-        onClose={[() => alert('취소'), () => alert('완료')]}
+        onCancel={() => alert('취소')}
+        onConfirm={() => alert('완료')}
       />
     </div>
   )
