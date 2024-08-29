d47835aea1a2e264dd9e8594001e98a097bf0f3c
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-29 22:39:45 +0900

    refactor: SubHeader 컴포넌트 변경 (#51)
    - PR 리뷰 반영하여 3가지 버전으로 분리

--- src/components/header/SubHeader.tsx
+++ src/components/header/SubHeader.tsx
@@ -4,47 +4,48 @@ import { Icon } from '../icons'
 
 export type HeaderProps = {
   title: string
-  canBack?: boolean
-  canClose?: boolean
   onClose?: VoidFunction[]
-  btnStyle?: 'icon' | 'text' | 'mixed'
 }
 
-export const SubHeader = ({
-  title,
-  canBack,
-  canClose,
-  onClose,
-  btnStyle = 'icon',
-}: HeaderProps) => {
+export const Back = ({ title }: HeaderProps) => {
   const router = useRouter()
   const handleBack = () => {
     router.back()
   }
 
-  const isIcon = btnStyle === 'icon'
-
   return (
     <header className="flex-between-align relative">
-      {
-        <button
-          type="button"
-          onClick={isIcon ? handleBack : onClose?.[0]}
-          className="body-B text-gray-6"
-        >
-          {canBack ? <Icon name="arrow-left" /> : !isIcon && '취소'}
-        </button>
-      }
+      <button onClick={handleBack}>
+        <Icon name="arrow-left" />
+      </button>
       <h1 className="subtitle-B absolute left-1/2 -translate-x-1/2 text-black">{title}</h1>
-      {canClose && (
-        <button
-          type="button"
-          onClick={isIcon ? onClose?.[0] : onClose?.[1]}
-          className="body-B text-black"
-        >
-          {isIcon ? <Icon name="close" /> : '완료'}
-        </button>
-      )}
     </header>
   )
 }
+
+export const Confirm = ({ title, onClose }: HeaderProps) => {
+  return (
+    <header className="flex-between-align relative">
+      <button className="body-B text-gray-6" onClick={onClose?.[0]}>
+        취소
+      </button>
+      <h1 className="subtitle-B absolute left-1/2 -translate-x-1/2 text-black">{title}</h1>
+      <button className="body-B text-black" onClick={onClose?.[1]}>
+        완료
+      </button>
+    </header>
+  )
+}
+
+export const Close = ({ title, onClose }: HeaderProps) => {
+  return (
+    <header className="flex-align relative justify-end">
+      <h1 className="subtitle-B absolute left-1/2 -translate-x-1/2 text-black">{title}</h1>
+      <button onClick={onClose?.[0]}>
+        <Icon name="close" />
+      </button>
+    </header>
+  )
+}
+
+export const SubHeader = { Back, Confirm, Close }
