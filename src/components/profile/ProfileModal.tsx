9471024db2088bff6aa3a49e6972723b92192efc
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-08-30 12:26:11 +0900

    feat: ProfileModal 컴포넌트 변경 (#89)
    - zIndex 설정
    - Portal 태그 제거

--- src/components/profile/ProfileModal.tsx
+++ src/components/profile/ProfileModal.tsx
@@ -3,9 +3,8 @@
 import type { MouseEventHandler } from 'react'
 import Link from 'next/link'
 
+import { zIndex } from '@/constants'
 import { useUserStore } from '@/store'
-
-import { Portal } from '../portal/Portal'
 
 import { Profile } from './Profile'
 
@@ -22,30 +21,28 @@ export const ProfileModal = ({ onClickProfileModal }: ProfileModalProps) => {
   }
 
   return (
-    <Portal>
-      <button
-        type="button"
-        onClick={handleClickScrim}
-        className={`absolute inset-0 flex h-[calc(100vh-75px)] w-full items-end justify-center overflow-hidden bg-[rgba(15,23,42,0.5)] px-[52px] pb-[62px]`}
-      >
-        <button type="button" onClick={handleClickScrim} className="grid grid-cols-3 gap-[40px]">
-          {user.family?.map(({ userId, username }) => {
-            const handleClickProfile = () => {
-              setUser({ ...user, userId: userId, username: username })
-              if (onClickProfileModal) onClickProfileModal()
-            }
+    <button
+      type="button"
+      onClick={handleClickScrim}
+      className={`absolute inset-0 flex h-[calc(100vh-75px)] w-full items-end justify-center overflow-hidden bg-[rgba(15,23,42,0.5)] px-[52px] pb-[62px] ${zIndex.fab}`}
+    >
+      <button type="button" onClick={handleClickScrim} className="grid grid-cols-3 gap-[40px]">
+        {user.family?.map(({ userId, username }) => {
+          const handleClickProfile = () => {
+            setUser({ ...user, userId: userId, username: username })
+            if (onClickProfileModal) onClickProfileModal()
+          }
 
-            return (
-              <div className="headline-B flex-column-align gap-y-[6px] text-white" key={userId}>
-                {username}
-                <Link href={`/home/${userId}`}>
-                  <Profile name={username} size="lg" onClickProfile={handleClickProfile} />
-                </Link>
-              </div>
-            )
-          })}
-        </button>
+          return (
+            <div className="headline-B flex-column-align gap-y-[6px] text-white" key={userId}>
+              {username}
+              <Link href={`/home/${userId}`}>
+                <Profile name={username} size="lg" onClickProfile={handleClickProfile} />
+              </Link>
+            </div>
+          )
+        })}
       </button>
-    </Portal>
+    </button>
   )
 }
