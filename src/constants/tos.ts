dded05349b96803a099af784fec5dbdc191cbc6d
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-09-06 08:48:16 +0900

    feat: 이용약관 관련 상수 추가 (#93)

---
+++ src/constants/tos.ts
@@ -0,0 +1,5 @@
+export const personal = `개인정보이용 동의할텨?\n\n\n\n\n\n\n\n개인정보이용 동의할텨?\n\n\n\n\n\n\n\n개인정보이용 동의할텨?\n\n\n\n\n\n\n\n개인정보이용 동의할텨?\n\n\n\n\n\n\n\n개인정보이용 동의할텨?\n\n\n\n\n\n\n\n개인정보이용 동의할텨?\n\n\n\n\n\n\n\n개인정보이용 동의할텨?\n\n\n\n`
+
+export const notification = `알림 수신 동의할텨?\n\n\n\n\n\n\n\n알림 수신 동의할텨?\n\n\n\n\n\n\n\n알림 수신 동의할텨?\n\n\n\n\n\n\n\n알림 수신 동의할텨?\n\n\n\n\n\n\n\n알림 수신 동의할텨?\n\n\n\n\n\n\n\n알림 수신 동의할텨?\n\n\n\n\n\n\n\n알림 수신 동의할텨?\n\n\n\n`
+
+export const tos = { personal, notification }
