509db931f063b02a40486a05e5e20518cd92d42b
김동균 <wooheuy5752@nate.com>
2024-09-05 21:27:02 +0900

    feat : ClinicWrite페이지 취소 및 완료 버튼 함수 추가 (#96)

--- src/features/clinic/write/ClinicWriteClientPage.tsx
+++ src/features/clinic/write/ClinicWriteClientPage.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { FormProvider } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
 
 import { MainHeader } from '@/components/header/MainHeader'
 import { useClinicForm } from '@/schema'
@@ -9,15 +10,24 @@ import { ClinicForm } from './ClinicForm'
 
 export const ClinicWriteClientPage = () => {
   const formMethod = useClinicForm()
+  const router = useRouter()
   const { handleSubmit } = formMethod
+
+  const handleClickCancle = () => {
+    router.back()
+  }
+
+  const handleClickConfirm = () => {
+    // 폼 제출 시 실행할 로직
+  }
 
   return (
     <div className="flex-column h-full overflow-hidden">
       <div>
         <MainHeader.Confirm
           title={`의사에게 전달할\n특이사항을 기입해주세요.`}
-          onCancel={() => {}}
-          onConfirm={handleSubmit(() => {})}
+          onCancel={handleClickCancle}
+          onConfirm={handleSubmit(handleClickConfirm)}
         />
       </div>
 
