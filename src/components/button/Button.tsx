ef99a8f53c220165e8c48b9cdf47e9be9cee5529
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-09-06 08:46:03 +0900

    fix: Button 컴포넌트 변경 (#93)
    - disable 시 스타일 지정

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,7 +3,7 @@
 import type { PropsWithChildren } from 'react'
 
 type ButtonProps = {
-  onClick: () => void
+  onClick?: () => void
   size?: 'md' | 'sm'
   primary?: boolean
   disabled?: boolean
@@ -18,7 +18,7 @@ export const Button = ({
   disabled = false,
   type = 'button',
 }: PropsWithChildren<ButtonProps>) => {
-  const color = primary ? 'bg-mint-6 text-white' : 'bg-gray-3 text-gray-7'
+  const color = !disabled ? 'bg-mint-6 text-white' : 'bg-gray-3 text-gray-7'
   const buttonSize = size === 'md' ? 'subtitle-B h-[56px]' : 'headline-B h-[52px]'
   return (
     <button
