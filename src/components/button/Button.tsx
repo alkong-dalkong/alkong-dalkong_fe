bf8ff7b2ad61b59941fd3692791bc3b549741184
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-09-10 17:54:43 +0900

    style: Button 컴포넌트 disabled 스타일 추가 (#93)

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -14,11 +14,15 @@ export const Button = ({
   children,
   onClick,
   size = 'md',
-  primary = false,
+  primary = true,
   disabled = false,
   type = 'button',
 }: PropsWithChildren<ButtonProps>) => {
-  const color = !disabled ? 'bg-mint-6 text-white' : 'bg-gray-3 text-gray-7'
+  const color = disabled
+    ? 'bg-gray-5 text-white'
+    : primary
+      ? 'bg-mint-6 text-white'
+      : 'bg-gray-3 text-gray-7'
   const buttonSize = size === 'md' ? 'subtitle-B h-[56px]' : 'headline-B h-[52px]'
   return (
     <button
