16bf104186ad4490840fbcfc9d3b293854343419
김동균 <wooheuy5752@nate.com>
2024-09-11 21:29:54 +0900

    refactor : ClinicClientPage 페이지 구조 변경 (#96)
    ScheduleSection에서 ScheduleList로 변경

--- src/features/clinic/ui/ClinicClientPage.tsx
+++ src/features/clinic/ui/ClinicClientPage.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { BottomNav, Calendar, Profile } from '@/components'
+import { useRouter } from 'next/navigation'
+
+import { BottomNav, Calendar, Label, Profile } from '@/components'
+import ActionTag from '@/components/actionTag/ActionTag'
+import { ScheduleList } from '@/features'
 import { useUserStore } from '@/store'
 
 import { useScheduleList } from '../service/useScheduleList'
-
-import { ScheduleSection } from './ScheduleSection'
 
 type ClinicClientPageProps = {
   userId: string
@@ -13,7 +15,12 @@ type ClinicClientPageProps = {
 
 export const ClinicClientPage = ({ userId }: ClinicClientPageProps) => {
   const { user } = useUserStore()
+  const router = useRouter()
   const scheduleList = useScheduleList(userId)
+
+  const handleClickPlusButton = () => {
+    router.push(`/clinic/${userId}/write`)
+  }
 
   return (
     <>
@@ -26,7 +33,13 @@ export const ClinicClientPage = ({ userId }: ClinicClientPageProps) => {
           <Calendar />
         </section>
 
-        <ScheduleSection userId={userId} scheduleList={scheduleList} />
+        <section className="mt-7">
+          <div className="flex-between mb-3">
+            <Label icon="clinic-label">병원 내원 일정</Label>
+            <ActionTag.Plus label="추가" primary onClick={handleClickPlusButton} />
+          </div>
+          <ScheduleList userId={userId} scheduleList={scheduleList} />
+        </section>
       </main>
       <BottomNav />
     </>
--- src/features/clinic/ui/ScheduleList.tsx
+++ src/features/clinic/ui/ScheduleList.tsx
@@ -1,9 +1,5 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-
-import { Label } from '@/components'
-import ActionTag from '@/components/actionTag/ActionTag'
 import { useCurrentDate } from '@/store'
 import type { ScheduleType } from '@/types'
 
@@ -14,22 +10,12 @@ type ClinicListProps = {
   scheduleList: ScheduleType[]
 }
 
-export const ScheduleSection = ({ userId, scheduleList }: ClinicListProps) => {
-  const router = useRouter()
+export const ScheduleList = ({ userId, scheduleList }: ClinicListProps) => {
   const date = useCurrentDate()
   const todaySchedules = scheduleList.filter((item) => item.hospitalDate.startsWith(date))
 
-  const handleClickPlusButton = () => {
-    router.push(`/clinic/${userId}/write`)
-  }
-
   return (
-    <section className="mt-7">
-      <div className="flex-between mb-3">
-        <Label icon="clinic-label">병원 내원 일정</Label>
-        <ActionTag.Plus label="추가" primary onClick={handleClickPlusButton} />
-      </div>
-
+    <>
       {todaySchedules.length ? (
         <div className="flex-column gap-3">
           {todaySchedules.map((item) => (
@@ -39,6 +25,6 @@ export const ScheduleSection = ({ userId, scheduleList }: ClinicListProps) => {
       ) : (
         <p className="subtitle-M mt-[82px] text-center text-gray-6">예정된 일정이 없어요!</p>
       )}
-    </section>
+    </>
   )
 }
