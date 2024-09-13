2efdc529e178203ab3d8d961084a79df06b30acc
g0mteng <gomteng03@naver.com>
2024-09-13 23:00:58 +0900

    feat: useGraph 추가 (#84)

---
+++ src/hooks/useGraph.ts
@@ -0,0 +1,31 @@
+'use client'
+
+import { useMemo, useState } from 'react'
+
+import type { WeightInfoType } from '@/types'
+
+export const useGraph = (info: WeightInfoType) => {
+  const [index, setIndex] = useState<number>(0)
+
+  const groupedData = useMemo(() => {
+    const groups = []
+    for (let i = 0; i < info.length; i += 4) {
+      groups.push(info.slice(i, i + 4))
+    }
+    return groups
+  }, [info])
+
+  const increase = () => {
+    if (index < groupedData.length - 1) {
+      setIndex((prev) => prev + 1)
+    }
+  }
+
+  const decrease = () => {
+    if (index > 0) {
+      setIndex((prev) => prev - 1)
+    }
+  }
+
+  return { data: groupedData[index], increase, decrease }
+}
