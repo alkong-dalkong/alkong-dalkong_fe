529c150fbec0e4aa70f29a33a8380856ef8d648e
김동균 <wooheuy5752@nate.com>
2024-09-14 14:53:37 +0900

    refactor : ClinicForm 분리 (#116)

--- src/features/clinic/ui/ClinicCreateClientPage.tsx
+++ src/features/clinic/ui/ClinicCreateClientPage.tsx
@@ -3,21 +3,28 @@
 import { FormProvider } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 
+import { ActionTag, InputGroup, Label, Tag } from '@/components'
 import { MainHeader } from '@/components/header/MainHeader'
-import { useSubmitAddClinicForm } from '@/features'
+import {
+  AlarmBottomSheet,
+  DateBottomSheet,
+  TagBottomSheet,
+  useSubmitAddClinicForm,
+} from '@/features'
+import { useToggle } from '@/hooks'
 import { useClinicForm } from '@/schema'
-
-import { ClinicForm } from './ClinicForm'
 
 export const ClinicCreateClientPage = () => {
   const router = useRouter()
   const formMethod = useClinicForm()
-  const { handleSubmit } = formMethod
-  const submitFormattedForm = useSubmitAddClinicForm()
+  const { getValues, handleSubmit } = formMethod
 
-  const handleClickCancle = () => {
-    router.back()
-  }
+  const [tagSheet, toggleTagSheet] = useToggle(false)
+  const [dateSheet, toggleDateSheet] = useToggle(false)
+  const [alarmSheet, toggleAlarmSheet] = useToggle(false)
+
+  const submitFormattedForm = useSubmitAddClinicForm()
+  const handleClickCancle = () => router.back()
 
   return (
     <div className="flex-column h-full overflow-hidden">
@@ -30,7 +37,64 @@ export const ClinicCreateClientPage = () => {
       </div>
 
       <FormProvider {...formMethod}>
-        <ClinicForm />
+        <form className="flex-column gap-8 overflow-scroll px-5 py-8 scrollbar-hide">
+          <InputGroup>
+            <Label icon="check-label">진료 과목</Label>
+            <div className="flex flex-wrap gap-2">
+              {getValues('medicalPart')?.map((part: string) => <Tag key={part} label={part} />)}
+              <ActionTag.Plus label="추가" onClick={toggleTagSheet} />
+            </div>
+            <InputGroup.ErrorMessage section="medicalPart" />
+            <TagBottomSheet
+              section="medicalPart"
+              isShowing={tagSheet}
+              onClickScrim={toggleTagSheet}
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <Label icon="calendar-label">방문 날짜</Label>
+            <button type="button" onClick={toggleDateSheet}>
+              <InputGroup.Input
+                section="hospitalDate"
+                readOnly
+                placeholder="방문 날짜를 선택해주세요."
+              />
+            </button>
+            <InputGroup.ErrorMessage section="hospitalDate" />
+            <DateBottomSheet
+              section="hospitalDate"
+              isShowing={dateSheet}
+              onClickScrim={toggleDateSheet}
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <Label icon="clinic-label">방문 병원</Label>
+            <InputGroup.Input section="hospitalName" placeholder="병원을 입력해주세요." />
+            <InputGroup.ErrorMessage section="hospitalName" />
+          </InputGroup>
+
+          <InputGroup>
+            <Label icon="emergency-label">증상 및 특이사항</Label>
+            <InputGroup.TextArea section="medicalMemo" placeholder="증상을 입력해주세요." />
+          </InputGroup>
+
+          <InputGroup>
+            <Label icon="time-label">알람</Label>
+            <div className="flex-between-align w-full rounded-xl border border-mint-3 py-4 pl-5 pr-4">
+              <Label>알람</Label>
+              <InputGroup.TextWithArrow section="medicalAlarm" onClick={toggleAlarmSheet} />
+            </div>
+
+            <InputGroup.ErrorMessage section="medicalAlarm" />
+            <AlarmBottomSheet
+              section="medicalAlarm"
+              isShowing={alarmSheet}
+              onClickScrim={toggleAlarmSheet}
+            />
+          </InputGroup>
+        </form>
       </FormProvider>
     </div>
   )
--- src/features/clinic/ui/ClinicForm.tsx
+++ src/features/clinic/ui/ClinicForm.tsx
@@ -2,28 +2,15 @@
 import { useFormContext } from 'react-hook-form'
 
 import { ActionTag, InputGroup, Label, Tag } from '@/components'
-import {
-  AlarmBottomSheet,
-  DateBottomSheet,
-  TagBottomSheet,
-  useClinicFormBottomSheet,
-} from '@/features'
+import { AlarmBottomSheet, DateBottomSheet, TagBottomSheet } from '@/features'
+import { useToggle } from '@/hooks'
 
-type ClinicFormProps = {
-  isReadOnly?: boolean
-}
-
-export const ClinicForm = ({ isReadOnly = false }: ClinicFormProps) => {
+export const ClinicForm = () => {
   const { getValues } = useFormContext()
 
-  const {
-    tagSheet,
-    toggleTagSheet,
-    dateSheet,
-    handletoggleDateSheet,
-    alarmSheet,
-    handleToggleAlarmSheet,
-  } = useClinicFormBottomSheet(isReadOnly)
+  const [tagSheet, toggleTagSheet] = useToggle(false)
+  const [dateSheet, toggleDateSheet] = useToggle(false)
+  const [alarmSheet, toggleAlarmSheet] = useToggle(false)
 
   return (
     <form className="flex-column gap-8 overflow-scroll px-5 py-8 scrollbar-hide">
@@ -31,7 +18,7 @@ export const ClinicForm = ({ isReadOnly = false }: ClinicFormProps) => {
         <Label icon="check-label">진료 과목</Label>
         <div className="flex flex-wrap gap-2">
           {getValues('medicalPart')?.map((part: string) => <Tag key={part} label={part} />)}
-          {!isReadOnly && <ActionTag.Plus label="추가" onClick={toggleTagSheet} />}
+          <ActionTag.Plus label="추가" onClick={toggleTagSheet} />
         </div>
         <InputGroup.ErrorMessage section="medicalPart" />
         <TagBottomSheet section="medicalPart" isShowing={tagSheet} onClickScrim={toggleTagSheet} />
@@ -39,7 +26,7 @@ export const ClinicForm = ({ isReadOnly = false }: ClinicFormProps) => {
 
       <InputGroup>
         <Label icon="calendar-label">방문 날짜</Label>
-        <button type="button" onClick={handletoggleDateSheet}>
+        <button type="button" onClick={toggleDateSheet}>
           <InputGroup.Input
             section="hospitalDate"
             readOnly
@@ -50,40 +37,32 @@ export const ClinicForm = ({ isReadOnly = false }: ClinicFormProps) => {
         <DateBottomSheet
           section="hospitalDate"
           isShowing={dateSheet}
-          onClickScrim={handletoggleDateSheet}
+          onClickScrim={toggleDateSheet}
         />
       </InputGroup>
 
       <InputGroup>
         <Label icon="clinic-label">방문 병원</Label>
-        <InputGroup.Input
-          section="hospitalName"
-          placeholder="병원을 입력해주세요."
-          readOnly={isReadOnly}
-        />
+        <InputGroup.Input section="hospitalName" placeholder="병원을 입력해주세요." />
         <InputGroup.ErrorMessage section="hospitalName" />
       </InputGroup>
 
       <InputGroup>
         <Label icon="emergency-label">증상 및 특이사항</Label>
-        <InputGroup.TextArea
-          section="medicalMemo"
-          placeholder={isReadOnly ? '특이사항이 없습니다.' : '증상을 입력해주세요.'}
-          readOnly={isReadOnly}
-        />
+        <InputGroup.TextArea section="medicalMemo" placeholder="증상을 입력해주세요." />
       </InputGroup>
 
       <InputGroup>
         <Label icon="time-label">알람</Label>
         <div className="flex-between-align w-full rounded-xl border border-mint-3 py-4 pl-5 pr-4">
           <Label>알람</Label>
-          <InputGroup.TextWithArrow section="medicalAlarm" onClick={handleToggleAlarmSheet} />
+          <InputGroup.TextWithArrow section="medicalAlarm" onClick={toggleAlarmSheet} />
         </div>
         <InputGroup.ErrorMessage section="medicalAlarm" />
         <AlarmBottomSheet
           section="medicalAlarm"
           isShowing={alarmSheet}
-          onClickScrim={handleToggleAlarmSheet}
+          onClickScrim={toggleAlarmSheet}
         />
       </InputGroup>
     </form>
