d81398615e11296c45994c7d83bb930685325c3f
김동균 <wooheuy5752@nate.com>
2024-09-14 14:31:22 +0900

    refactor : `/clinic/info`를 '/clinic/edit`과 `/clinic/info`로 분리 (#116)

---
+++ src/app/clinic/[userId]/edit/[medicalId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ClinicEditClientPage } from '@/features'
+
+const ClinicEdit = () => {
+  return <ClinicEditClientPage />
+}
+
+export default ClinicEdit
--- src/features/clinic/ui/ClinicInfoClientPage.tsx
+++ src/features/clinic/ui/ClinicInfoClientPage.tsx
@@ -1,20 +1,32 @@
 'use client'
 
 import { FormProvider } from 'react-hook-form'
+import { useParams, useRouter } from 'next/navigation'
 
-import { ClinicForm, ClinicInfoHeader, ClinicInfoModal, useInsertedClinicForm } from '@/features'
-import { useBoolean, useToggle } from '@/hooks'
+import { MainHeader } from '@/components'
+import { ClinicForm, ClinicInfoModal, useInsertedClinicForm } from '@/features'
+import { useBoolean } from '@/hooks'
 
 export const ClinicInfoClientPage = () => {
+  const router = useRouter()
   const formMethod = useInsertedClinicForm()
-  const [isEdit, toggleIsEdit] = useToggle(false)
+
   const [modalState, openModal, closeModal] = useBoolean(false)
+  const { userId, medicalId } = useParams<{ userId: string; medicalId: string }>()
+
+  const handleClickModify = () => router.push(`/clinic/${userId}/edit/${medicalId}`)
 
   return (
     <div className="flex-column h-full overflow-hidden">
       <FormProvider {...formMethod}>
-        <ClinicInfoHeader isEdit={isEdit} toggleIsEdit={toggleIsEdit} openModal={openModal} />
-        <ClinicForm isReadOnly={!isEdit} />
+        <div>
+          <MainHeader.Modify
+            title={`의사에게 전달할\n특이사항을 기입해주세요.`}
+            onDelete={openModal}
+            onModify={handleClickModify}
+          />
+        </div>
+        <ClinicForm isReadOnly />
       </FormProvider>
 
       <ClinicInfoModal modalState={modalState} closeModal={closeModal} />
