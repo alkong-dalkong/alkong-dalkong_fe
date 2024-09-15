8227073789e83a27ebe246366a636e4f5c5932a6
g0mteng <gomteng03@naver.com>
2024-09-15 16:59:14 +0900

    feat: DUMMY 데이터 삭제 (#122)

--- src/features/home/ui/HomePage.tsx
+++ src/features/home/ui/HomePage.tsx
@@ -16,44 +16,8 @@ export const HomePage = ({ userId }: { userId: string }) => {
     )
   }
 
-  const DUMMY = {
-    code: 200,
-    data: {
-      upcomingMedicalInfo: {
-        hospitalName: '연세 세브란스 정형외과',
-        hospitalDate: '2024-09-17 12:00',
-        medicalPart: ['건강검진'],
-      },
-      recentMedicalInfo: {
-        hospitalName: '바른 이비인후과',
-        hospitalDate: '2024-06-25 09:00',
-      },
-      recentWeightInfo: {
-        weight: 63.5,
-        date: '2024-08-24',
-      },
-      currentMedicineInfo: [
-        {
-          medicineName: '혈압약',
-          times: ['07:30', '20:00'],
-          weekList: ['MONDAY'],
-        },
-        {
-          medicineName: '종합비타민',
-          times: ['13:00'],
-          weekList: ['MONDAY'],
-        },
-        {
-          medicineName: '철분제',
-          times: ['08:00', '12:00', '20:00'],
-          weekList: ['MONDAY', 'THURSDAY', 'SUNDAY'],
-        },
-      ],
-    },
-  }
-
   const { upcomingMedicalInfo, recentMedicalInfo, recentWeightInfo, currentMedicineInfo } =
-    DUMMY.data // homePageData.data
+    homePageData.data
 
   return (
     <DashBoardTemplate route="home">
