45c5f4eeb459a14ea977af0c3e711b451b7ea63a
김동균 <wooheuy5752@nate.com>
2024-09-19 12:06:24 +0900

    feat : 폼에서 사용될 상수 선언 (#126)

---
+++ src/constants/medicine.ts
@@ -0,0 +1,14 @@
+export const WEEKDAYS = ['월', '화', '수', '목', '금']
+export const WEEKENDS = ['토', '일']
+export const EVERYDAY = ['일', '월', '화', '수', '목', '금', '토']
+export const EN_DAYS = [
+  'SUNDAY',
+  'MONDAY',
+  'TUESDAY',
+  'WEDNESDAY',
+  'THURSDAY',
+  'FRIDAY',
+  'SATURDAY',
+]
+
+export const MEDICINE_ALARM_TIME = ['1시간 전', '30분 전', '10분전', '없음']
