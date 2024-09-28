4475a334aabd7553543707e4ea6d590b30521518
g0mteng <gomteng03@naver.com>
2024-09-28 11:59:25 +0900

    fix: generateStaticParmas 사용 (#129)

--- src/app/onboarding/[step]/page.tsx
+++ src/app/onboarding/[step]/page.tsx
@@ -1,27 +1,26 @@
+import { notFound } from 'next/navigation'
+
 import { OnboardingWrapper } from '@/features'
 
-type OnboardingStaticProps = {
+type OnboardingPageProps = {
   params: {
     step: string
   }
 }
 
-export async function getStaticPaths() {
-  return {
-    paths: Array.from(
-      { length: 3 },
-      (_, idx): OnboardingStaticProps => ({
-        params: {
-          step: `${idx + 1}`,
-        },
-      }),
-    ),
-    fallback: false,
-  }
+export async function generateStaticParams() {
+  return Array.from({ length: 3 }, (_, idx) => ({
+    step: `${idx + 1}`,
+  }))
 }
 
-const OnboardingPage = ({ params: { step } }: OnboardingStaticProps) => {
-  return <OnboardingWrapper step={Number(step)} />
+const OnboardingPage = ({ params: { step } }: OnboardingPageProps) => {
+  const stepNumber = Number(step)
+  if (!(stepNumber === 1 || stepNumber === 2 || stepNumber === 3)) {
+    return notFound()
+  }
+
+  return <OnboardingWrapper step={stepNumber} />
 }
 
 export default OnboardingPage
