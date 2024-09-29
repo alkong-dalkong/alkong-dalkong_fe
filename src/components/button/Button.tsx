772bc3742dbc6ba765f8baa14320388e17da0cf1
g0mteng <gomteng03@naver.com>
2024-09-29 10:20:23 +0900

    refactor: NativeButtonType 적용 (#131)

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,34 +2,30 @@
 
 import type { PropsWithChildren } from 'react'
 
-type ButtonProps = {
-  onClick?: () => void
+type NativeButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>
+type ButtonProps = NativeButtonProps & {
   size?: 'md' | 'sm'
   primary?: boolean
   disabled?: boolean
-  type?: 'button' | 'submit'
 }
 
 export const Button = ({
   children,
-  onClick,
   size = 'md',
   primary = true,
   disabled = false,
-  type = 'button',
+  ...nativeButtonProps
 }: PropsWithChildren<ButtonProps>) => {
-  const color = disabled
-    ? 'bg-gray-5 text-white'
-    : primary
-      ? 'bg-mint-6 text-white'
-      : 'bg-gray-3 text-gray-7'
+  const color = !disabled && primary ? 'bg-mint-6 text-white' : 'bg-gray-3 text-gray-7'
   const buttonSize = size === 'md' ? 'subtitle-B h-[56px]' : 'headline-B h-[52px]'
   return (
     <button
-      type={type}
       className={`flex-center rounded-[12px] ${color} w-full ${buttonSize}`}
-      onClick={onClick}
       disabled={disabled}
+      {...nativeButtonProps}
     >
       {children}
     </button>
