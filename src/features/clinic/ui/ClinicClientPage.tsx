a49736f77064f405fe12a521ee6d1e4a19905588
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-10-26 18:27:43 +0900

    feat: ClinicClientPage 컴포넌트 변경 (#111)
    - 헤더 내 프로필 클릭 시 설정 페이지로 이동
    - 헤더 내 프로필명 user.name -> user.ownerName 변경

--- src/features/clinic/ui/ClinicClientPage.tsx
+++ src/features/clinic/ui/ClinicClientPage.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+
 import { BottomNav, Calendar, Profile } from '@/components'
 import { ScheduleList, useMonthlyScheduleList } from '@/features'
 import { useUserStore } from '@/store'
@@ -8,11 +10,21 @@ export const ClinicClientPage = () => {
   const { user } = useUserStore()
   const scheduleList = useMonthlyScheduleList()
 
+  const router = useRouter()
+  const handleGoSetting = () => {
+    router.push(`/setting`)
+  }
+
   return (
     <>
       <main className="mx-4 mb-[130px] mt-[38px] overflow-y-scroll scrollbar-hide">
         <div className="absolute right-5 top-[22px]">
-          <Profile name={user.name} size="sm" bgColor="#C5FDEC" />
+          <Profile
+            name={user.ownerName}
+            size="sm"
+            bgColor="#C5FDEC"
+            onClickProfile={handleGoSetting}
+          />
         </div>
 
         <section>
