1f5895843977296129eaad304a60afecb384a694
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-10-26 18:56:53 +0900

    feat: CodeModal, InviteModal에 Portal 적용 (#111)

--- src/features/setting/ui/SettingModal.tsx
+++ src/features/setting/ui/SettingModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon, Modal } from '@/components'
+import { Button, Icon, Modal, Portal } from '@/components'
 import type { CodeModalProps, InviteModalProps } from '@/types'
 
 import { copyClipboard } from '../utils/copyClipboard'
@@ -12,42 +12,46 @@ export const CodeModal = ({
   code,
 }: CodeModalProps) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex-column-align w-full px-[20px]">
-        <h1 className="subtitle-B mb-[8px] text-black">{title}</h1>
-        <h2 className="headline-M mb-[24px] whitespace-pre text-center text-black">
-          {description}
-        </h2>
-        <div className="flex-center mb-[16px] w-full rounded-xl bg-mint-1 py-[11px] text-center text-mint-9">
-          {code}
+    <Portal>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <div className="flex-column-align w-full px-[20px]">
+          <h1 className="subtitle-B mb-[8px] text-black">{title}</h1>
+          <h2 className="headline-M mb-[24px] whitespace-pre text-center text-black">
+            {description}
+          </h2>
+          <div className="flex-center mb-[16px] w-full rounded-xl bg-mint-1 py-[11px] text-center text-mint-9">
+            {code}
+          </div>
+          <div className="body-M flex gap-[6px] text-mint-7">
+            <button onClick={() => copyClipboard(code)}>코드 복사하기</button>
+            <Icon name="line-bar" color="#0E8763" />
+            <button onClick={() => shareCode(code)}>코드 공유하기</button>
+          </div>
         </div>
-        <div className="body-M flex gap-[6px] text-mint-7">
-          <button onClick={() => copyClipboard(code)}>코드 복사하기</button>
-          <Icon name="line-bar" color="#0E8763" />
-          <button onClick={() => shareCode(code)}>코드 공유하기</button>
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+    </Portal>
   )
 }
 
 export const InviteModal = ({ isOpen, onConfirm, onClose, inviter }: InviteModalProps) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex-column-align w-full px-[16px]">
-        <h1 className="subtitle-B mb-[8px] text-black">
-          {inviter ? `${inviter}님의 그룹에 초대받았어요!` : '그룹에 초대받았어요!'}
-        </h1>
-        <h2 className="headline-M mb-[24px]">해당 가족에 들어가시겠습니까?</h2>
-        <div className="flex w-full gap-[15px]">
-          <Button size="sm" type="submit" onClick={onConfirm}>
-            네
-          </Button>
-          <Button primary={false} size="sm" onClick={onClose}>
-            아니오
-          </Button>
+    <Portal>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <div className="flex-column-align w-full px-[16px]">
+          <h1 className="subtitle-B mb-[8px] text-black">
+            {inviter ? `${inviter}님의 그룹에 초대받았어요!` : '그룹에 초대받았어요!'}
+          </h1>
+          <h2 className="headline-M mb-[24px]">해당 가족에 들어가시겠습니까?</h2>
+          <div className="flex w-full gap-[15px]">
+            <Button size="sm" type="submit" onClick={onConfirm}>
+              네
+            </Button>
+            <Button primary={false} size="sm" onClick={onClose}>
+              아니오
+            </Button>
+          </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </Portal>
   )
 }
