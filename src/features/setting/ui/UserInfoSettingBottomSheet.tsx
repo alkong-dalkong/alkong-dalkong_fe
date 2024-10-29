32c670e7f0e20875c936526db179bb9f7f65a486
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2024-10-29 23:04:45 +0900

    feat: UserInfoSettingBottomSheet 변경 (#111)
    - 편집모드 아닐 시, 헤더 UI 반영
    - 편집모드 아닐 시, 편집 불가능 적용

--- src/features/setting/ui/UserInfoSettingBottomSheet.tsx
+++ src/features/setting/ui/UserInfoSettingBottomSheet.tsx
@@ -4,6 +4,7 @@ import type { SubmitHandler } from 'react-hook-form'
 import { FormProvider } from 'react-hook-form'
 
 import { BottomSheet, DevTool, InputGroup, Label, Profile, SubHeader } from '@/components'
+import { useToggle } from '@/hooks'
 import { useAccountEditForm } from '@/schema'
 import { useUserStore } from '@/store'
 import type { CustomBottomSheetProps, UserInfoFormType } from '@/types'
@@ -11,6 +12,7 @@ import type { CustomBottomSheetProps, UserInfoFormType } from '@/types'
 import { useEditUserInfo } from '../query/useSetting'
 
 export const UserInfoSettingBottomSheet = ({ isShowing, onClickScrim }: CustomBottomSheetProps) => {
+  const [isEditMode, toggleEditMode] = useToggle(false)
   const { user, setUser } = useUserStore()
 
   const { mutate: editUserInfo } = useEditUserInfo()
@@ -26,7 +28,7 @@ export const UserInfoSettingBottomSheet = ({ isShowing, onClickScrim }: CustomBo
     editUserInfo(editUserInfoData, {
       onSuccess: () => {
         setUser({ ...user, name: editUserInfoData.name, ownerName: editUserInfoData.name })
-        onClickScrim()
+        toggleEditMode()
       },
     })
   }
@@ -35,11 +37,15 @@ export const UserInfoSettingBottomSheet = ({ isShowing, onClickScrim }: CustomBo
     <>
       <BottomSheet isShowing={isShowing} onClickScrim={onClickScrim}>
         <div className="size-full overflow-y-scroll scrollbar-hide">
-          <SubHeader.Confirm
-            title="나의 정보 수정"
-            onCancel={onClickScrim}
-            onConfirm={handleSubmit(editUserInfoHandler)}
-          />
+          {!isEditMode ? (
+            <SubHeader.Modify title="나의 정보" onModify={toggleEditMode} onClose={onClickScrim} />
+          ) : (
+            <SubHeader.Confirm
+              title="나의 정보 수정"
+              onCancel={onClickScrim}
+              onConfirm={handleSubmit(editUserInfoHandler)}
+            />
+          )}
           <main className="flex-column-align pb-[55px]">
             <div className="mb-[16px] mt-[27px]">
               <Profile name={user.ownerName} size="2xl" bgColor="#C5FDEC" />
@@ -48,22 +54,34 @@ export const UserInfoSettingBottomSheet = ({ isShowing, onClickScrim }: CustomBo
               <form className="flex-column w-full gap-[16px]">
                 <InputGroup>
                   <Label>이름</Label>
-                  <InputGroup.Input section="name" placeholder="성명을 입력해주세요." />
+                  <InputGroup.Input
+                    section="name"
+                    placeholder="성명을 입력해주세요."
+                    readOnly={!isEditMode}
+                  />
                   <InputGroup.ErrorMessage section="name" />
                 </InputGroup>
                 <InputGroup>
                   <Label>휴대전화번호</Label>
-                  <InputGroup.Input section="phoneNumber" placeholder="숫자만 입력해주세요." />
+                  <InputGroup.Input
+                    section="phoneNumber"
+                    placeholder="숫자만 입력해주세요."
+                    readOnly={!isEditMode}
+                  />
                   <InputGroup.ErrorMessage section="phoneNumber" />
                 </InputGroup>
                 <InputGroup>
                   <Label>생년월일</Label>
-                  <InputGroup.Input section="birth" placeholder="생년월일 8자리를 입력해주세요." />
+                  <InputGroup.Input
+                    section="birth"
+                    placeholder="생년월일 8자리를 입력해주세요."
+                    readOnly={!isEditMode}
+                  />
                   <InputGroup.ErrorMessage section="birth" />
                 </InputGroup>
                 <InputGroup>
                   <Label>성별</Label>
-                  <InputGroup.Gender />
+                  <InputGroup.Gender disabled={!isEditMode} />
                   <InputGroup.ErrorMessage section="gender" />
                 </InputGroup>
               </form>
